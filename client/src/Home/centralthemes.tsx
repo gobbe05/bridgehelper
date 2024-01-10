20927de3af70dd3e5ef89298221e96d9387eb7c2
gobbe05 <gabriel@raskov.se>
2024-01-10 08:19:42 +0100

    Updated the theme component and did some spacing on the central themes and all themes components

--- client/src/Home/centralthemes.tsx
+++ client/src/Home/centralthemes.tsx
@@ -7,15 +7,19 @@ import { Link } from "react-router-dom"
 function CentralThemes() {
     const color = useContext(colorcontext)
     return (
-        <div className={`flex flex-col justify-center gap-2 px-8 lg:px-16 py-8 rounded mb-24 ${color && color.colorMode == "light" ? "bg-poker text-white" : "bg-white text-poker"}`}>
+        <div className={`flex flex-col px-8 lg:px-16 py-8 rounded mb-24 ${color && color.colorMode == "light" ? "bg-poker text-white" : "bg-white text-poker"}`}>
             <h1 className="text-3xl font-bold">Centrala Teman</h1>
-            <Theme tema="Tema 4b" heading="Mer o NT-givar"/>
-            <Theme tema="Tema 4b" heading="Mer o NT-givar"/>
-            <Theme tema="Tema 4b" heading="Mer o NT-givar"/>
-            <Theme tema="Tema 4b" heading="Mer o NT-givar"/>
-            <Theme tema="Tema 4b" heading="Mer o NT-givar"/>
+            
+            <div className="flex flex-col justify-center gap-2 ">
+                <Theme tema="Tema 4b" heading="Mer o NT-givar"/>
+                <Theme tema="Tema 4b" heading="Mer o NT-givar"/>
+                <Theme tema="Tema 4b" heading="Mer o NT-givar"/>
+                <Theme tema="Tema 4b" heading="Mer o NT-givar"/>
+                <Theme tema="Tema 4b" heading="Mer o NT-givar"/>
+            </div>
+ 
 
-            <Link to="/allateman" className={`flex items-center text-xl border-b ml-auto mt-4 sm:hover:px-2 ${color && color.colorMode == "light" ? "text-blue-300 border-blue-300" : "text-blue-500 border-blue-500"} transition-all`}>Visa alla teman <BsArrowRightShort /></Link>
+            <Link to="/allateman" className={`flex items-center text-xl border-b ml-auto mt-8 sm:hover:px-2 ${color && color.colorMode == "light" ? "text-blue-300 border-blue-300" : "text-blue-500 border-blue-500"} transition-all`}>Visa alla teman <BsArrowRightShort /></Link>
         </div>
     )
 }
