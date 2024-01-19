b11842918423b36769bef168aca8a9b2db21eb5b
gobbe05 <92081333+gobbe05@users.noreply.github.com>
2024-01-19 11:19:14 +0000

    Created routes : GET - admin with message GET - admin with error GET - edittheme with id GET - edittheme with id and error GET - edithighlight GET - createtheme with error GET - createtheme with id GET - getthemes GET - getcentralthemes
    POST - updatetheme with id
    POST - edithighlight
    
    ANY - 404 to client.php
    
    Removed route :
    ANY - 404 with views/4040.php

--- index.php
+++ index.php
@@ -5,12 +5,23 @@ get('/', 'client.php');
 get('/login', 'views/login.php');
 get('/logout', 'api/logout.php');
 get('/admin', 'views/admin.php');
+get('/admin/$message', 'views/admin.php');
+get('/admin/$error', 'views/admin.php');
+get('/edittheme/$id', 'views/update.php');
+get('/edittheme/$id/$error', 'views/update.php');
+get('/edithighlight', 'views/edithighlight.php');
 get('/createtheme', 'views/createtheme.php');
+get('/createtheme/$error', 'views/createtheme.php');
+get('/deletetheme/$id', 'api/deletetheme.php');
 get('/visitcount', 'api/visitcount.php');
+get('/getthemes', 'api/getthemes.php');
+get('/getcentralthemes', 'api/getcentralthemes.php');
 
 post("/login", 'api/login.php');
 post('/createtheme', 'api/createtheme.php');
+post('/updatetheme/$id', 'api/updatetheme.php');
+post('/edithighlight', 'api/edithighlight.php');
 
-any('/404','views/404.php');
+any('/404','client.php');
 
 ?>
