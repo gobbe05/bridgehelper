84b2efd30a0cad9601ec994921fe4b59219cab3f
gobbe05 <92081333+gobbe05@users.noreply.github.com>
2024-01-19 11:28:42 +0000

    Created getcentralthemes API file

---
+++ api/getcentralthemes.php
@@ -0,0 +1,10 @@
+<?php 
+include 'db.php';
+
+$sql = "SELECT * FROM Themes WHERE Highlight=1";
+$result = $mysqli -> query($sql);
+$themes = mysqli_fetch_all($result, MYSQLI_ASSOC); # all rows to array
+echo json_encode($themes);
+$mysqli -> close();
+exit();
+?>
