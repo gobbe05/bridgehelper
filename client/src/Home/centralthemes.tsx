70fc8b9cc70990a61171cc9ea2278f77e85bec88
gobbe05 <92081333+gobbe05@users.noreply.github.com>
2024-01-20 14:41:00 +0000

    Changed centralthemes.tsx component to serve content dynamically

--- client/src/Home/centralthemes.tsx
+++ client/src/Home/centralthemes.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import { colorcontext } from "../main"
 import { BsArrowRightShort } from "react-icons/bs"
 import Theme from "../Components/theme"
@@ -6,22 +6,34 @@ import { Link } from "react-router-dom"
 
 function CentralThemes() {
     const color = useContext(colorcontext)
-    return (
-        <div className={`flex flex-col px-8 lg:px-16 py-8 rounded mb-24 ${color && color.colorMode == "light" ? "bg-poker text-white" : "bg-white text-poker"}`}>
-            <h1 className="text-3xl font-bold">Centrala Teman</h1>
-            
-            <div className="flex flex-col justify-center gap-2 ">
-                <Theme tema="Tema 4b" heading="Mer o NT-givar"/>
-                <Theme tema="Tema 4b" heading="Mer o NT-givar"/>
-                <Theme tema="Tema 4b" heading="Mer o NT-givar"/>
-                <Theme tema="Tema 4b" heading="Mer o NT-givar"/>
-                <Theme tema="Tema 4b" heading="Mer o NT-givar"/>
-            </div>
- 
+    const [centralThemes, setCentralThemes] = useState<Array<any>>([])
 
-            <Link to="/allateman" className={`flex items-center text-xl border-b ml-auto mt-8 sm:hover:px-2 ${color && color.colorMode == "light" ? "text-blue-300 border-blue-300" : "text-blue-500 border-blue-500"} transition-all`}>Visa alla teman <BsArrowRightShort /></Link>
-        </div>
-    )
+    const FetchCentralThemes = async () => {
+        const response = await fetch('/getcentralthemes');
+        const data = await response.json();
+        setCentralThemes(data);
+    }
+
+    useEffect(() => {
+        FetchCentralThemes();
+    }, [])
+    if(centralThemes.length > 0) {
+        return (
+            <div className={`flex flex-col px-8 lg:px-16 py-8 rounded mb-24 ${color && color.colorMode == "light" ? "bg-poker text-white" : "bg-white text-poker"} transition-all`}>
+                <h1 className="text-3xl font-bold">Centrala Teman</h1>
+
+                <div className="flex flex-col justify-center gap-2 ">
+                    {centralThemes.map((theme) => (
+                        <Theme tema={theme.Theme} heading={theme.Name} file={theme.File}/>
+                    ))}
+                </div>
+                    
+
+                <Link to="/allateman" className={`flex items-center text-xl border-b ml-auto mt-8 sm:hover:px-2 ${color && color.colorMode == "light" ? "text-blue-300 border-blue-300" : "text-blue-500 border-blue-500"} transition-all`}>Visa alla teman <BsArrowRightShort /></Link>
+            </div>
+        )
+    }
+    return <></>
 }
 
 export default CentralThemes
