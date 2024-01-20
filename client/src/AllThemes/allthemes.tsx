d2a12272ff137e9a7a89d688471e08c71efdf946
gobbe05 <92081333+gobbe05@users.noreply.github.com>
2024-01-20 14:34:40 +0000

    Changed allthemes.tsx component to save data dynamically

--- client/src/AllThemes/allthemes.tsx
+++ client/src/AllThemes/allthemes.tsx
@@ -1,25 +1,36 @@
-import { useContext } from "react"
+import { useContext, useEffect } from "react"
 import Theme from "../Components/theme"
 import { colorcontext } from "../main"
+import { useState } from "react"
 
 function AllThemes() {
     const color = useContext(colorcontext)
-    return (
-        <>
-            <div className={`flex flex-col justify-center gap-2 p-8 lg:p-16 rounded mb-24 ${color && color.colorMode == "light" ? "bg-poker text-white" : "bg-white text-poker"} transition-all`}>
-                <h1 className="text-3xl font-bold">Alla Teman</h1>
-                <Theme tema="Tema 1" heading="Mer om NT givar"/>
-                <Theme tema="Tema 1" heading="Mer om NT givar"/>
-                <Theme tema="Tema 1" heading="Mer om NT givar"/>
-                <Theme tema="Tema 1" heading="Mer om NT givar"/>
-                <Theme tema="Tema 1" heading="Mer om NT givar"/>
-                <Theme tema="Tema 1" heading="Mer om NT givar"/>
-                <Theme tema="Tema 1" heading="Mer om NT givar"/>
-                <Theme tema="Tema 1" heading="Mer om NT givar"/>
-                <Theme tema="Tema 1" heading="Mer om NT givar"/>
-            </div>
-        </>
-    )
+    const [themes, setThemes] = useState<Array<any>>([])
+
+    async function GetThemes() {
+        const response = await fetch("/getthemes")
+        const data = await response.json()
+        setThemes(data);
+    }
+
+    useEffect(() => {
+        GetThemes()
+    }, [])
+    if(themes.length > 0) {
+        return (
+            <>
+                <div className={`flex flex-col justify-center gap-2 p-8 lg:p-16 rounded mb-24 ${color && color.colorMode == "light" ? "bg-poker text-white" : "bg-white text-poker"} transition-all`}>
+                    <h1 className="text-3xl font-bold">Alla Teman</h1>
+                    {themes.map((theme) => (
+                      <div>
+                        <Theme tema={theme.Theme} heading={theme.Name} file={theme.File}/>  
+                      </div>
+                    ))}
+                </div>
+            </>
+        )
+    }
+    return <></>
 }
 
 export default AllThemes
