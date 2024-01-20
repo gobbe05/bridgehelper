da8ff3da3b06938f94d2cef791e575e635b262f0
gobbe05 <92081333+gobbe05@users.noreply.github.com>
2024-01-20 14:32:51 +0000

    Cleanded up visitcount code

--- api/visitcount.php
+++ api/visitcount.php
@@ -1,11 +1,12 @@
 <?php 
-
 include "db.php";
 
 $sql = "SELECT COUNT(*) FROM Visits";
-
 $result = $mysqli->query($sql);
 
-$data=mysqli_fetch_assoc($result);
+$data = mysqli_fetch_assoc($result);
+
 echo json_encode($data["COUNT(*)"]);
+$mysqli -> close();
+exit();
 ?>
