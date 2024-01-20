e2f9768a6f53543fb2aa5493a9027b0f387bb5b8
gobbe05 <92081333+gobbe05@users.noreply.github.com>
2024-01-20 14:35:19 +0000

    Changed theme.tsx component to have a downloadable file. Chabged thene.tsx component to show theme name.

--- client/src/Components/theme.tsx
+++ client/src/Components/theme.tsx
@@ -2,16 +2,19 @@ import { useContext } from "react"
 import { colorcontext } from "../main"
 import { IoArrowDownOutline } from "react-icons/io5";
 
-function Theme({tema, heading} : {tema: string, heading: string}) {
+function Theme({tema, heading, file} : {tema: string, heading: string, file: string}) {
     const color = useContext(colorcontext)
     return (
         <>
-            <div className={`flex items-center border-b ${color?.colorMode == "light" ? "border-white" : "border-poker"} py-4 mt-2 group cursor-pointer`}>
+            <a href={file} className={`flex items-center border-b ${color?.colorMode == "light" ? "border-white" : "border-poker"} py-2 mt-2 group cursor-pointer`}>
                 <p className="text-lg">{heading}</p>
-                <div className={`flex justify-center items-center border-2  w-12 aspect-square rounded-full ml-auto ${color?.colorMode == "dark" ? "border-poker group-hover:bg-poker group-hover:text-white" : "border-white group-hover:bg-white group-hover:text-poker"} transition-all`}>
-                    <IoArrowDownOutline size={32}/>
+                <div className="flex flex-col justify-center ml-auto ">
+                    <div className={`flex justify-center items-center border-2 mx-auto w-12 aspect-square rounded-full ${color?.colorMode == "dark" ? "border-poker group-hover:bg-poker group-hover:text-white" : "border-white group-hover:bg-white group-hover:text-poker"} transition-all`}>
+                        <IoArrowDownOutline size={32}/>
+                    </div>
+                    <p className="text-center text-sm">{tema}</p>
                 </div>
-            </div>
+            </a>
         </>
     )
 }
