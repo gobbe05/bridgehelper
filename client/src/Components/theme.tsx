3464c34711e014e6e55024832c1bf67daf6d113a
Gabriel Raskov <gabriel@raskov.se>
2024-03-07 10:27:06 +0100

    __target blank on links

--- client/src/Components/theme.tsx
+++ client/src/Components/theme.tsx
@@ -6,7 +6,7 @@ function Theme({tema, heading, file} : {tema: string, heading: string, file: str
     const color = useContext(colorcontext)
     return (
         <>
-            <a href={file} className={`flex items-center border-b ${color?.colorMode == "light" ? "border-white" : "border-gray-700"} py-2 mt-2 group cursor-pointer`}>
+            <a href={file} target="__blank" className={`flex items-center border-b ${color?.colorMode == "light" ? "border-white" : "border-gray-700"} py-2 mt-2 group cursor-pointer`}>
                 <p className="text-lg">{heading}</p>
                 <div className="flex flex-col justify-center ml-auto ">
                     <div className={`flex justify-center items-center border-2 mx-auto p-1 aspect-square rounded-full ${color?.colorMode == "dark" ? "border-gray-700 group-hover:bg-gray-700 group-hover:text-white" : "border-white group-hover:bg-white group-hover:text-gray-700"} transition-all`}>
