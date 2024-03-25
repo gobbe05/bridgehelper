71f860c6a05e7298c200720fb4edceef129df689
Gabriel Raskov <gabriel@raskov.se>
2024-03-25 11:41:12 +0100

    Text changes

--- client/src/Home/introduction.tsx
+++ client/src/Home/introduction.tsx
@@ -7,10 +7,8 @@ function Introduction() {
     return (
         <div className={`flex items-center rounded ${color?.colorMode == "light" ? "sm:bg-poker sm:text-white" : "sm:bg-white sm:text-gray-700"}`}>
             <div className={`sm:w-1/2  sm:p-8 transition-all`}>
-                <h1 className="text-3xl font-bold">Information</h1>
-                <p className="text-lg">Hjälpredan består av 30 klickbara temaavsnitt syftande till att spegla Bridgefundamenta.</p>
-                <p className="font-light">(Du som känner dig mer bekväm med att
-se informationen på papper – skriv ut materialet).</p>
+                <h1 className="text-3xl font-bold">Kom Igång</h1>
+                <p className="text-xl font-semibold">Kom igång med att repetera dina Bridgekunskaper med hjälp av BridgeHelper, som består av 30 klickbara temaavsnitt syftande till att spegla Bridgefundamenta.</p>
             </div>
             <div className="sm:w-1/2">
                 <img className="hidden sm:block h-48 mx-auto rounded" src="https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/5626618/deck-of-cards-clipart-sm.png"/>
--- client/src/Home/temainformation.tsx
+++ client/src/Home/temainformation.tsx
@@ -8,8 +8,8 @@ function InformationTema() {
         <div className={`flex items-center rounded ${color?.colorMode == "light" ? "sm:bg-poker sm:text-white" : "sm:bg-white sm:text-gray-700"}`}>
             <div className={`sm:p-8 rounded`}>
                 <h1 className="text-3xl font-bold">Så här kan du använda Bridgehelper</h1>
-                <p className="text-xl mt-8 font-semibold">Varje tema innehåller en kortversion om 2 högst 3 sidor per tema, med några rubrikrader följt av en 2:a del i form av något mer omfattande redovisning, om behov skulle föreligga.</p>
-                <p className="text-xl mt-4 font-bold">Början av denna del 2 är markerad i gul</p>
+                <p className="text-xl mt-8 font-semibold">Varje tema innehåller en kortversion om 2 till 3 sidor innehållande några rubrikrader och en koncentrerad genomgång av innehållet i temat. Därefter följer en del 2 med en mer omfattande redovisning om behov skulle föreligga.</p>
+                <p className="text-xl mt-4 font-bold">Början av denna del 2 är markerad med gul färg</p>
             </div>
         </div>
     )
