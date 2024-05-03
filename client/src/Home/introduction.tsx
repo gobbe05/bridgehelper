98857cb430d8b561938458c056ff36f9e3c3df57
Gabriel Raskov <gabriel@raskov.se>
2024-05-03 12:52:55 +0200

    Added text for PDFs

--- client/src/Home/introduction.tsx
+++ client/src/Home/introduction.tsx
@@ -8,7 +8,7 @@ function Introduction() {
         <div className={`flex items-center rounded ${color?.colorMode == "light" ? "sm:bg-poker sm:text-white" : "sm:bg-white sm:text-gray-700"}`}>
             <div className={`sm:w-1/2  sm:p-8 transition-all`}>
                 <h1 className="text-3xl font-bold">Kom Igång</h1>
-                <p className="text-xl font-semibold">Kom igång med att repetera dina Bridgekunskaper med hjälp av BridgeHelper, som består av 30 klickbara temaavsnitt syftande till att spegla Bridgefundamenta.</p>
+                <p className="text-lg mt-2">Kom igång med att repetera dina Bridgekunskaper med hjälp av BridgeHelper, som består av 30 klickbara temaavsnitt syftande till att spegla Bridgefundamenta.</p>
             </div>
             <div className="sm:w-1/2">
                 <img className="hidden sm:block h-48 mx-auto rounded" src="https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/5626618/deck-of-cards-clipart-sm.png"/>
