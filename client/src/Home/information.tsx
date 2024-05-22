1575ae9f2a882b3d644af0738f3c3f95796b7aa5
Gabriel Raskov <gabriel@raskov.se>
2024-05-22 14:41:09 +0200

    Updated link on front page

--- client/src/Home/information.tsx
+++ client/src/Home/information.tsx
@@ -15,7 +15,7 @@ function Information() {
                     <Theme pdf={"/uploads/4b Mer om NT-öppning.pdf"} heading={"Behöver du endast en uppfräschning av NT-budet och vill ha en överblick på principen för överföring, går du direkt till tema 4b"} tema={"Tema 4b"}/>
                     <Theme pdf={"/uploads/6 Tumregler & Hörnstolpar.pdf"} heading={"Behöver du endast snegla på ett antal Bridge-komihåg-ramsor går du direkt till tema 6​​​"} tema={"Tema 6"}/>
                     <Theme pdf={"/uploads/14 (a-b) En rundtur med budkedjan Öh1-Sh1-Öh2.pdf"} heading={"Behöver du endast en överblick på kombinationer med de första buden Öh1, Sh1 och Öh2, går du direkt till tema 14"} tema={"Tema 14"}/>
-                    <Theme pdf={"/uploads/16 Om öppning med något av buden 1X el 2NT el 2Kl.pdf"} heading={"Behöver du endast en överblick på alternativ till öppningsbud vid spelstark hand (Hp=18+) går du direkt till tema 16​​"} tema={"Tema 16"}/>
+                    <Theme pdf={"/uploads/16  Om öppning med något av buden 1X el 2NT el 2Kl.pdf"} heading={"Behöver du endast en överblick på alternativ till öppningsbud vid spelstark hand (Hp=18+) går du direkt till tema 16​​"} tema={"Tema 16"}/>
                     <Theme pdf={"/uploads/30 Digital  Hjälpreda i Bridge . Ord och begrepp i Bridge.pdf"} heading={"Behöver du endast tillgång till en alfabetisk bridge-ordlista, ​​​​går du direkt till tema 30.  ​​"} tema={"Tema 30"}/>
                 </div>
                 <p></p>
